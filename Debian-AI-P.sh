eca700b998b197799ee07da7c0f59717b48ce725
Brayman30 <33497147+Brayman30@users.noreply.github.com>
2021-06-30 16:35:05 -0400

    Create Debian-AI-P.sh

---
+++ Debian-AI-P.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This script installs many different programs including: ano firefox python vscode discord gparted gimp thunar viewnior mousepad vlc 
+zenity --password --title="Enter Password to Install Programs" --timeout=10 | sudo -S 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install aptitude
+sudo aptitude install nano firefox python vscode discord gparted gimp thunar viewnior mousepad vlc
